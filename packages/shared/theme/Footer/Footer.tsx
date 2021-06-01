b3f224e68eed4503613669af0326057816545d99
dd-platformcy <demetris.diogenous@platform.cy>
2021-06-01 19:51:39 +0300

    Playing around

---
+++ packages/shared/theme/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react'
+
+export const Footer: FunctionComponent = () => (
+  <>
+    {/*<BottomNavigation
+      onChange={() => {
+        console.log('on change...')
+      }}
+      showLabels
+      className={classes.bottomNavigation}
+    >
+      Footer A
+    </BottomNavigation>*/}
+  </>
+)
