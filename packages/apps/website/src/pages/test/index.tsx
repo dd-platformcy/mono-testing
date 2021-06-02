cddb1e36974cbe8523e658b178adeeac174aa9f2
dd-platformcy <demetris.diogenous@platform.cy>
2021-06-02 08:28:24 +0300

    Playing around

---
+++ packages/apps/website/src/pages/test/index.tsx
@@ -0,0 +1,10 @@
+import React, {FunctionComponent} from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            Hello this is a test.
+        </div>
+    )
+}
+export default Test
